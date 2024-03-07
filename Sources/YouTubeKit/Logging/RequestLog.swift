a524666296d50d2875bf9208af538803fa37c77e
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2024-03-07 20:01:41 +0100

    Gave more properties to GenericRequestLog.

--- Sources/YouTubeKit/Logging/RequestLog.swift
+++ Sources/YouTubeKit/Logging/RequestLog.swift
@@ -9,37 +9,51 @@
 import Foundation
 
 /// A protocol whose sole purpose is to have the ability to store the logs into an array in ``RequestsLogger/logs``.
-public protocol GenericRequestLog {    
+public protocol GenericRequestLog {
+    associatedtype LogType: YouTubeResponse
+    typealias RequestParameters = [HeadersList.AddQueryInfo.ContentTypes : String]
+    
     /// The id of the log, can be used to remove it from ``RequestsLogger/logs`` using ``RequestsLogger/clearLogWithId(_:)`` or ``RequestsLogger/clearLogsWithIds(_:)``.
     var id: UUID { get }
     
     /// The date when the request has been finished (data has been received and processed).
     var date: Date { get }
+    
+    /// The request parameters provided in ``YouTubeModel/sendRequest(responseType:data:useCookies:result:)``.
+    var providedParameters: RequestParameters { get }
+    
+    /// The request that has been sent over the network, can be nil if the request couldn't even be made, for example if one of the given RequestParameter didn't pass the verification tests.
+    var request: URLRequest? { get }
+    
+    /// The raw data from the response.
+    var responseData: Data? { get }
+    
+    /// The type of the request
+    var expectedResultType: LogType.Type { get }
+    
+    /// The processed result or an error if there was one during the process.
+    var result: Result<LogType, Error> { get }
  }
 
 /// A structure representing a log.
 public struct RequestLog<ResponseType: YouTubeResponse>: Identifiable, GenericRequestLog {
-    public typealias RequestParameters = [HeadersList.AddQueryInfo.ContentTypes : String]
-
+    public typealias LogType = ResponseType
+    
     public let id = UUID()
     
     public let date = Date()
     
-    /// The request parameters provided in ``YouTubeModel/sendRequest(responseType:data:useCookies:result:)``.
     public let providedParameters: RequestParameters
     
-    /// The request that has been sent over the network, can be nil if the request couldn't even be made, for example if one of the given RequestParameter didn't pass the verification tests.
     public let request: URLRequest?
     
-    /// The raw data from the response.
     public let responseData: Data?
     
-    public let expectedResultType = ResponseType.self
+    public let expectedResultType = LogType.self
     
-    /// The processed result or an error if there was one during the process.
-    public let result: Result<ResponseType, Error>
+    public let result: Result<LogType, Error>
     
-    public init(providedParameters: RequestParameters, request: URLRequest?, responseData: Data?, result: Result<ResponseType, Error>) {
+    public init(providedParameters: RequestParameters, request: URLRequest?, responseData: Data?, result: Result<LogType, Error>) {
         self.providedParameters = providedParameters
         self.request = request
         self.responseData = responseData
