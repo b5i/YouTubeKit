6564b8c143760b66390527fce752ee12d4d41960
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2025-01-21 22:19:37 +0100

    fix: fix "login required" for lot of videos

--- Sources/YouTubeKit/YouTubeModel.swift
+++ Sources/YouTubeKit/YouTubeModel.swift
@@ -417,16 +417,14 @@ public class YouTubeModel {
                     .init(name: "Accept", content: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),
                     .init(name: "Accept-Encoding", content: "gzip, deflate, br"),
                     .init(name: "Host", content: "www.youtube.com"),
-                    .init(name: "User-Agent", content: "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)"),
+                    .init(name: "User-Agent", content: "com.google.ios.youtube/20.02.3 (iPhone16,2; U; CPU iOS 18_3 like Mac OS X;)"),
                     .init(name: "Accept-Language", content: "\(self.selectedLocale);q=0.9"),
                     .init(name: "Origin", content: "https://www.youtube.com/"),
                     .init(name: "Referer", content: "https://www.youtube.com/"),
                     .init(name: "Content-Type", content: "application/json"),
                     .init(name: "X-Origin", content: "https://www.youtube.com"),
                     .init(name: "X-Youtube-Client-Name", content: "5"),
-                    .init(name: "X-Youtube-Client-Version", content: "19.45.4"),
-                    .init(name: "Cookie", content: "PREF=hl=\(self.selectedLocaleLanguageCode)&tz=UTC; SOCS=CAI; GPS=1; VISITOR_INFO1_LIVE=X454mME5IB0; VISITOR_PRIVACY_METADATA=CgJDSBIEGgAgKQ%3D%3D") // yt-dlp
-                ],
+                    .init(name: "X-Youtube-Client-Version", content: "19.45.4")                ],
                 addQueryAfterParts: [
                     .init(index: 0, encode: false, content: .query)
                 ],
@@ -437,14 +435,14 @@ public class YouTubeModel {
                       "context": {
                         "client": {
                           "clientName": "IOS",
-                          "clientVersion": "19.45.4",
+                          "clientVersion": "20.02.3",
                           "deviceMake": "Apple",
                           "deviceModel": "iPhone16,2",
                           "hl": "\#(self.selectedLocaleLanguageCode)",
                           "osName": "iPhone",
-                          "osVersion": "iOS 18_1",
+                          "osVersion": "18.3.0.22D5040d",
                           "timeZone": "UTC",
-                          "userAgent": "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1 like Mac OS X)",
+                          "userAgent": "com.google.ios.youtube/20.02.3 (iPhone16,2; U; CPU iOS 18_3 like Mac OS X;)",
                           "utcOffsetMinutes": 0
                         }
                       },
