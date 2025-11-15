f18c6b30a3f7b29178e8d2a5aeaca9a9ecf6e728
alexandr potapov <alex.potapov.dev00@gmail.com>
2025-11-15 18:28:39 +0300

    Rename targets to YouTubeKitParser to avoid conflicts

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "YouTubeKitParser",
     products: [
         .library(
-            name: "YouTubeKitParser",  // Новое имя
-            targets: ["YouTubeKit"]
+            name: "YouTubeKitParser",
+            targets: ["YouTubeKitParser"]
         ),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YouTubeKit",
+            name: "YouTubeKitParser",
             dependencies: []/*,
             swiftSettings: [.unsafeFlags(["-Xfrontend", "-strict-concurrency=complete"])]*/),
         .testTarget(
